5a8719a9134f95ae8069095c6118bfb8f957e9b3
Maycon Rayone <maycon.rayone@gmail.com>
2020-05-09 21:56:01 -0300

    incluindo path correto

--- src/config/Upload.ts
+++ src/config/Upload.ts
@@ -2,7 +2,7 @@ import multer from 'multer';
 import path from 'path';
 import crypto from 'crypto';
 
-const tmpDir = path.resolve('../', '../', 'tmp');
+const tmpDir = path.resolve(__dirname, '../', '../', 'tmp');
 export default {
   homeDir: tmpDir,
   storage: multer.diskStorage({
